61b8e3248d9d3e37eced5f50494468c61f8ee617
Pedro <pedrosloiola@gmail.com>
2025-05-08 20:30:54 -0300

    Update no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
