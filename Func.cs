5c5b6bef685829e1639d2b0219c54bf711ab5dff
Pedro <pedrosloiola@gmail.com>
2025-05-08 21:26:25 -0300

    Add files via upload

---
+++ Func.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_DW
+{
+    internal class Func
+    {
+        static void Main()
+        {
+            Func<string, string, string> combinarNome = (nome, sobrenome) => $"{nome} {sobrenome}";
+
+            Func<string, string> paraMaiusculas = s => s.ToUpper();
+
+            Func<string, string> removerEspacos = s => s.Replace(" ", "");
+
+            string nomeFinal = removerEspacos(paraMaiusculas(combinarNome("luis", "bucciarati")));
+
+            Console.WriteLine(nomeFinal);
+        }
+    }
+}
